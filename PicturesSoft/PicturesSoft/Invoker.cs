f9f1358e38aaf28141ed4c778a2c98d557f0b40c
HarlamovBuldog <vladwoode@gmail.com>
2018-10-19 11:04:25 +0300

    Invoker.cs file was added, PicturesSoft.csproj file was edited
    This file contains DialogInvoker class which solves the problem with the OpenFileDialog.OpenFile() method, throwing the exception that the file is busy.
    Regular PicturesSoft.csproj file update.

---
+++ PicturesSoft/PicturesSoft/Invoker.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Windows.Forms;
+
+namespace PicturesSoft
+{
+    public class DialogInvoker
+    {
+        public OpenFileDialog InvokeDialog;
+        private Thread InvokeThread;
+        private DialogResult InvokeResult;
+
+        public DialogInvoker()
+        {
+            InvokeDialog = new OpenFileDialog();
+            InvokeThread = new Thread(new ThreadStart(InvokeMethod));
+            InvokeThread.SetApartmentState(ApartmentState.STA);
+            InvokeResult = DialogResult.None;
+        }
+
+        public DialogInvoker(string openFileDialogFilter)
+        {
+            InvokeDialog = new OpenFileDialog();
+            InvokeDialog.Filter = openFileDialogFilter;
+            InvokeThread = new Thread(new ThreadStart(InvokeMethod));
+            InvokeThread.SetApartmentState(ApartmentState.STA);
+            InvokeResult = DialogResult.None;
+        }
+        
+
+
+        public DialogResult Invoke()
+        {
+            InvokeThread.Start();
+            InvokeThread.Join();
+            return InvokeResult;
+        }
+
+        private void InvokeMethod()
+        {
+            InvokeResult = InvokeDialog.ShowDialog();
+        }
+    }
+}
