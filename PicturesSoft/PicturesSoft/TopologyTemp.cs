162a1d7130d545d43ab3619b37582ab68121bd8b
HarlamovBuldog <vladwoode@gmail.com>
2018-11-06 17:25:28 +0300

    TopologyTemp.cs file was added
    TopologyTemp was created for parsing json file. Properties names are the same as needed in .json file.

---
+++ PicturesSoft/PicturesSoft/TopologyTemp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicturesSoft
+{
+    public class TopologyTemp
+    {
+        public string topologyAddress { get; set; }
+
+        public string topologyPointIP { get; set; }
+
+        public string type { get; set; }
+
+    }
+}
