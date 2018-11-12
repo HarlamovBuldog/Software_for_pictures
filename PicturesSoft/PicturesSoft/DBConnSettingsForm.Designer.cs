73d4233070f0bf0bae3be796ebdfacd134bf3b11
HarlamovBuldog <vladwoode@gmail.com>
2018-11-12 09:43:35 +0300

    DBConnSettingsForm.cs and two related files were added
    DBConnSettingsForm.cs contains logic for making main local catalog file CashBoxesCatalog.xml. It includes 5 info fields for connection to database, checking if related "topology.structure" file exists (otherwise you will be unable to create  CashBoxesCatalog.xml file). reading and deserializing "topology.structure" json file and a lot of related error processing.

---
+++ PicturesSoft/PicturesSoft/DBConnSettingsForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace PicturesSoft
+{
+    partial class DBConnSettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwdLabel = new System.Windows.Forms.Label();
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.serverLabel = new System.Windows.Forms.Label();
+            this.ApplyDbSettingsBtn = new System.Windows.Forms.Button();
+            this.passwdTextBox = new System.Windows.Forms.TextBox();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.portIDTextBox = new System.Windows.Forms.TextBox();
+            this.serverIDTextBox = new System.Windows.Forms.TextBox();
+            this.dataBaseNameLabel = new System.Windows.Forms.Label();
+            this.dataBaseNameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // passwdLabel
+            // 
+            this.passwdLabel.AutoSize = true;
+            this.passwdLabel.Location = new System.Drawing.Point(45, 102);
+            this.passwdLabel.Name = "passwdLabel";
+            this.passwdLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwdLabel.TabIndex = 17;
+            this.passwdLabel.Text = "Password";
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(65, 76);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(29, 13);
+            this.userNameLabel.TabIndex = 16;
+            this.userNameLabel.Text = "User";
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(72, 50);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(26, 13);
+            this.portLabel.TabIndex = 15;
+            this.portLabel.Text = "Port";
+            // 
+            // serverLabel
+            // 
+            this.serverLabel.AutoSize = true;
+            this.serverLabel.Location = new System.Drawing.Point(60, 24);
+            this.serverLabel.Name = "serverLabel";
+            this.serverLabel.Size = new System.Drawing.Size(38, 13);
+            this.serverLabel.TabIndex = 14;
+            this.serverLabel.Text = "Server";
+            // 
+            // ApplyDbSettingsBtn
+            // 
+            this.ApplyDbSettingsBtn.Location = new System.Drawing.Point(104, 162);
+            this.ApplyDbSettingsBtn.Name = "ApplyDbSettingsBtn";
+            this.ApplyDbSettingsBtn.Size = new System.Drawing.Size(154, 32);
+            this.ApplyDbSettingsBtn.TabIndex = 13;
+            this.ApplyDbSettingsBtn.Text = "Применить";
+            this.ApplyDbSettingsBtn.UseVisualStyleBackColor = true;
+            this.ApplyDbSettingsBtn.Click += new System.EventHandler(this.ApplyDbSettingsBtn_Click);
+            // 
+            // passwdTextBox
+            // 
+            this.passwdTextBox.Location = new System.Drawing.Point(104, 99);
+            this.passwdTextBox.Name = "passwdTextBox";
+            this.passwdTextBox.Size = new System.Drawing.Size(170, 20);
+            this.passwdTextBox.TabIndex = 12;
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(104, 73);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(170, 20);
+            this.userNameTextBox.TabIndex = 11;
+            // 
+            // portIDTextBox
+            // 
+            this.portIDTextBox.Location = new System.Drawing.Point(104, 47);
+            this.portIDTextBox.Name = "portIDTextBox";
+            this.portIDTextBox.Size = new System.Drawing.Size(170, 20);
+            this.portIDTextBox.TabIndex = 10;
+            // 
+            // serverIDTextBox
+            // 
+            this.serverIDTextBox.Location = new System.Drawing.Point(104, 21);
+            this.serverIDTextBox.Name = "serverIDTextBox";
+            this.serverIDTextBox.Size = new System.Drawing.Size(170, 20);
+            this.serverIDTextBox.TabIndex = 9;
+            // 
+            // dataBaseNameLabel
+            // 
+            this.dataBaseNameLabel.AutoSize = true;
+            this.dataBaseNameLabel.Location = new System.Drawing.Point(45, 128);
+            this.dataBaseNameLabel.Name = "dataBaseNameLabel";
+            this.dataBaseNameLabel.Size = new System.Drawing.Size(53, 13);
+            this.dataBaseNameLabel.TabIndex = 19;
+            this.dataBaseNameLabel.Text = "Database";
+            // 
+            // dataBaseNameTextBox
+            // 
+            this.dataBaseNameTextBox.Location = new System.Drawing.Point(104, 125);
+            this.dataBaseNameTextBox.Name = "dataBaseNameTextBox";
+            this.dataBaseNameTextBox.Size = new System.Drawing.Size(170, 20);
+            this.dataBaseNameTextBox.TabIndex = 18;
+            // 
+            // DBConnSettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 209);
+            this.Controls.Add(this.dataBaseNameLabel);
+            this.Controls.Add(this.dataBaseNameTextBox);
+            this.Controls.Add(this.passwdLabel);
+            this.Controls.Add(this.userNameLabel);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.serverLabel);
+            this.Controls.Add(this.ApplyDbSettingsBtn);
+            this.Controls.Add(this.passwdTextBox);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.portIDTextBox);
+            this.Controls.Add(this.serverIDTextBox);
+            this.Name = "DBConnSettingsForm";
+            this.Text = "Настройка подключения к БД";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label passwdLabel;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.Label serverLabel;
+        private System.Windows.Forms.Button ApplyDbSettingsBtn;
+        private System.Windows.Forms.TextBox passwdTextBox;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TextBox portIDTextBox;
+        private System.Windows.Forms.TextBox serverIDTextBox;
+        private System.Windows.Forms.Label dataBaseNameLabel;
+        private System.Windows.Forms.TextBox dataBaseNameTextBox;
+    }
+}
---
+++ PicturesSoft/PicturesSoft/DBConnSettingsForm.cs
@@ -0,0 +1,232 @@
+﻿using Newtonsoft.Json;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace PicturesSoft
+{
+    public partial class DBConnSettingsForm : Form
+    {
+        public DBConnSettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyDbSettingsBtn_Click(object sender, EventArgs e)
+        {
+            string serverId = this.serverIDTextBox.Text;
+            string port = this.portIDTextBox.Text;
+            string userName = this.userNameTextBox.Text;
+            string passwd = this.passwdTextBox.Text;
+            string dataBaseName = this.dataBaseNameTextBox.Text;
+
+            //validation here
+
+            if(MakeLocalCatalog(serverId, port, userName, passwd, dataBaseName))
+            {
+                MessageBox.Show("Локальный каталог был успешно создан. Программа готова к работе!",
+                    "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+        }
+
+        public bool MakeLocalCatalog(string serverId, string port,
+            string user, string passwd, string dataBaseName)
+        {
+            List<Shop> shops = new List<Shop>();
+            List<CashBox> cashBoxes = new List<CashBox>();
+            List<TopologyTemp> topologyTemps;
+
+            string filepath = Path.Combine(Path.GetDirectoryName(
+                        Assembly.GetExecutingAssembly().Location), @"Data\topology.structure");
+
+            if(!File.Exists(filepath))
+            {
+                MessageBox.Show("Нет файла справочника! Перейдите в настройки сервера" +
+                    " подключения и загрузите его!", "Ошибка", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return false;
+            }
+
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                string json = r.ReadToEnd();
+                topologyTemps = new List<TopologyTemp>(JsonConvert.DeserializeObject<List<TopologyTemp>>(json));
+            }
+
+            int indexOfScndComa;
+            int indexofThirdComa;
+            string currentShopCode = String.Empty;
+
+            List<string> shopCodesCheck = new List<string>();
+
+            foreach (var tplg in topologyTemps)
+            {
+                if (tplg.type.Equals("CENTRUM"))
+                    continue;
+
+                var result = tplg.topologyAddress
+                        .Select((ch, index) => new { ch, index })
+                        .Where(x => x.ch == '.')
+                        .Skip(1)
+                        .FirstOrDefault();
+
+                indexOfScndComa = result.index;
+
+                indexofThirdComa = tplg.topologyAddress.LastIndexOf('.');
+
+                currentShopCode = tplg.topologyAddress
+                    .Substring(indexOfScndComa + 1, indexofThirdComa - indexOfScndComa - 1);
+
+                if (shopCodesCheck.Count != 0)
+                {
+                    if (!shopCodesCheck.Contains(currentShopCode))
+                    {
+                        shopCodesCheck.Add(currentShopCode);
+                        shops.Add(new Shop()
+                        {
+                            Code = String.Copy(currentShopCode)
+                        });
+                    }
+                }
+                else
+                {
+                    shopCodesCheck.Add(currentShopCode);
+                    shops.Add(new Shop()
+                    {
+                        Code = String.Copy(currentShopCode)
+                    });
+                }
+
+                cashBoxes.Add(new CashBox()
+                {
+                    IpAddress = tplg.topologyPointIP,
+                    Number = tplg.topologyAddress.Substring(indexofThirdComa + 1),
+                    ShopCode = String.Copy(currentShopCode)
+                });
+            }
+
+            foreach (Shop shop in shops)
+            {
+                foreach (CashBox cashBox in cashBoxes)
+                {
+                    if (shop.Code.Equals(cashBox.ShopCode))
+                    {
+                        shop.CashBoxes.Add(new CashBox()
+                        {
+                            IpAddress = String.Copy(cashBox.IpAddress),
+                            Number = String.Copy(cashBox.Number),
+                            ShopCode = String.Copy(cashBox.ShopCode)
+                        });
+                    }
+                }
+            }
+
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+            try
+            {
+                // PostgeSQL-style connection string
+                string connstring = String.Format("Server={0};Port={1};" +
+                    "User Id={2};Password={3};Database={4};",
+                    serverId, port, user, passwd, dataBaseName);
+                // Making connection with Npgsql provider
+                NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                // quite complex sql statement
+                string sql = "SELECT number, name, address FROM topology_shop";
+                // data adapter making request from our connection
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+                // i always reset DataSet before i do
+                // something with it.... i don't know why :-)
+                ds.Reset();
+                // filling DataSet with result from NpgsqlDataAdapter
+                da.Fill(ds);
+                // since it C# DataSet can handle multiple tables, we will select first
+                dt = ds.Tables[0];
+                // since we only showing the result we don't need connection anymore
+                conn.Close();
+            }
+            catch (Exception msg)
+            {
+                // something went wrong, and you wanna know why
+                MessageBox.Show(msg.ToString() + "\nВозникли проблемы с подключением к базе данных." +
+                    "Обратитесь к администратору либо проверьте введенные данные",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            DataRow[] foundRows;
+
+            foreach (Shop shop in shops)
+            {
+                foundRows = dt.Select("number = " + shop.Code);
+
+                shop.Name = foundRows[0]["name"].ToString();
+                shop.Address = foundRows[0]["address"].ToString();
+            }
+
+            if (!CreateLocalCatalogXmlFile(shops))
+                return false;
+
+            return true;
+        }
+
+        private bool CreateLocalCatalogXmlFile(List<Shop> shops)
+        {
+            XDocument xDoc = new XDocument();
+
+            //create shopListXElement
+            XElement shopListXElement = new XElement("shops");
+
+            XElement shopXElement;
+
+            foreach (Shop shop in shops)
+            {
+                shopXElement = new XElement("shop",
+                    new XAttribute("number", shop.Code),
+                    new XAttribute("name", shop.Name),
+                    new XAttribute("address", shop.Address)
+                    );
+
+                foreach (CashBox cashBox in shop.CashBoxes)
+                {
+                    shopXElement.Add(new XElement("cashBox",
+                        new XAttribute("ipAddress", cashBox.IpAddress),
+                        new XAttribute("number", cashBox.Number)
+                        ));
+                }
+
+                shopListXElement.Add(shopXElement);
+            }
+
+            //making shopListXElement to be root element
+            xDoc.Add(shopListXElement);
+
+            try
+            {
+                xDoc.Save("Data/CashBoxesCatalog.xml");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message + "Не удалось сохранить файл!", "Ошибка!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        //need to check connection here
+
+        //((Form1)this.Owner).MakeLocalCatalogCall(serverId,
+        //   port, userName, passwd, dataBaseName);
+    }
+}
