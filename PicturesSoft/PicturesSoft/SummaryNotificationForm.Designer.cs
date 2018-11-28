87299d60cb0e888dddb54a878ff3b384f2004450
HarlamovBuldog <vladwoode@gmail.com>
2018-11-28 10:50:00 +0300

    Create files SummaryNotificationForm.cs, SummaryNotificationForm.Designer.cs, SummaryNotificationForm.resx
    New SummaryNotification form was created. It creates pivot tables to notificate user about operations, which are going to be performed before uploading or downloading info from cash box, and also about results of downloading or uploading.

---
+++ PicturesSoft/PicturesSoft/SummaryNotificationForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace PicturesSoft
+{
+    partial class SummaryNotificationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupListControl1 = new GroupedListControl.GroupListControl();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // groupListControl1
+            // 
+            this.groupListControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupListControl1.AutoScroll = true;
+            this.groupListControl1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.groupListControl1.Location = new System.Drawing.Point(24, 12);
+            this.groupListControl1.Name = "groupListControl1";
+            this.groupListControl1.SingleItemOnlyExpansion = false;
+            this.groupListControl1.Size = new System.Drawing.Size(535, 430);
+            this.groupListControl1.TabIndex = 0;
+            this.groupListControl1.WrapContents = false;
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(115, 500);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(75, 23);
+            this.confirmButton.TabIndex = 2;
+            this.confirmButton.Text = "Ок";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(352, 499);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(90, 448);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.ReadOnly = true;
+            this.descriptionTextBox.Size = new System.Drawing.Size(408, 20);
+            this.descriptionTextBox.TabIndex = 4;
+            // 
+            // SummaryNotificationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 558);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.groupListControl1);
+            this.Name = "SummaryNotificationForm";
+            this.Text = "SummaryNotificationForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GroupedListControl.GroupListControl groupListControl1;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+    }
+}
---
+++ PicturesSoft/PicturesSoft/SummaryNotificationForm.cs
@@ -0,0 +1,60 @@
+﻿using GroupedListControl;
+using System.Windows.Forms;
+
+namespace PicturesSoft
+{
+    public partial class SummaryNotificationForm : Form
+    {
+        public SummaryNotificationForm(GroupListControl groupListControl, WorkMode workMode)
+        {
+            InitializeComponent();
+
+            switch(workMode.WorkType)
+            {
+                case WorkModeType.DownloadFromCashBoxAndShowNotificationTable:
+                    this.Text = "Подтверждение загрузки информации с кассы";
+                    this.descriptionTextBox.Text = "Подтвердите загрузку информации, указанной выше";
+                    break;
+                case WorkModeType.DownloadFromCashBoxAndShowCorrespondingResults:
+                    this.Text = "Результаты загрузки информации с кассы";
+                    this.descriptionTextBox.Text = "Выше показаны результаты загрузки информации с кассы." +
+                        "Если возникли ошибки, то просмотрите логи для более детального разбора" +
+                        " либо свяжитесь с системным администратором.";
+                    this.cancelButton.Visible = false;
+                    break;                
+                case WorkModeType.UploadToCashBoxAndShowNotificationTable:
+                    this.Text = "Подтверждение загрузки информации на кассу";
+                    this.descriptionTextBox.Text = "Подтвердите загрузку информации, указанной выше";
+                    break;
+                case WorkModeType.UploadToCashBoxAndShowCorrespondingResults:
+                    this.Text = "Результаты загрузки информации на кассу";
+                    this.descriptionTextBox.Text = "Выше показаны результаты загрузки информации на кассы." +
+                        "Если возникли ошибки, то просмотрите логи для более детального разбора" +
+                        " либо свяжитесь с системным администратором.";
+                    this.cancelButton.Visible = false;
+                    break;
+            }
+
+            int counter = groupListControl.Controls.Count;
+
+            for (int i = 0; i < counter; i++)
+            {
+                this.groupListControl1.Controls.Add(groupListControl.Controls[0]);
+            }
+
+            groupListControl1.Update();
+            groupListControl1.Invalidate(true);
+        }
+
+        private void confirmButton_Click(object sender, System.EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void cancelButton_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
