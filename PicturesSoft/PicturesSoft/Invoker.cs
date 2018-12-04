ab582e013b392e9bbefc1eb254bcaff06b50a277
HarlamovBuldog <vladwoode@gmail.com>
2018-12-04 17:18:27 +0300

    Update Invoker.cs
    Now class DialogInvoker is inherited from IDisposable interface.  Dispose method was overloaded to dispose OpenFileDialog field.

--- PicturesSoft/PicturesSoft/Invoker.cs
+++ PicturesSoft/PicturesSoft/Invoker.cs
@@ -1,9 +1,10 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PicturesSoft
 {
-    public class DialogInvoker
+    public class DialogInvoker:IDisposable
     {
         public OpenFileDialog InvokeDialog;
         private Thread InvokeThread;
@@ -37,5 +38,43 @@ namespace PicturesSoft
         {
             InvokeResult = InvokeDialog.ShowDialog();
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    this.InvokeDialog.Dispose();
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                this.InvokeDialog = null;
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        ~DialogInvoker() {
+          // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
     }
 }
